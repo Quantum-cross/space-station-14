b2324c91c46ec3dc94c8bd4be88f68138988edb0
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-21 22:39:37 -0400

    add animate on spawn component and system

---
+++ Content.Client/Animations/AnimateOnSpawnSystem.cs
@@ -0,0 +1,54 @@
+﻿using Robust.Client.GameObjects;
+using Robust.Shared.Timing;
+using Content.Shared.Animations;
+
+namespace Content.Client.Animations;
+
+public sealed class AnimateOnSpawnSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming Timing = default!;
+    [Dependency] private readonly SpriteSystem _spriteSystem = default!;
+    [Dependency] private readonly ILogManager _log = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
+
+    private ISawmill _sawmill = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AnimateOnSpawnComponent, AnimationCompletedEvent>(HandleAnimationCompleteEvent);
+        SubscribeLocalEvent<AnimateOnSpawnComponent, ComponentInit>(HandleInitEvent);
+
+        _sawmill = _log.GetSawmill("AnimateOnSpawnSystem");
+    }
+
+    private void HandleAnimationCompleteEvent(Entity<AnimateOnSpawnComponent> ent, ref AnimationCompletedEvent args)
+    {
+        _sawmill.Warning("Animation complete!");
+    }
+
+    private void HandleInitEvent(Entity<AnimateOnSpawnComponent> ent, ref ComponentInit args)
+    {
+        if (!TryComp<SpriteComponent>(ent, out var sprite))
+            return;
+
+        _appearanceSystem.SetData(ent, AnimateOnSpawnVisualState.State, true);
+        ent.Comp.EndTime = Timing.CurTime + ent.Comp.Delay;
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<AnimateOnSpawnComponent>();
+        while (query.MoveNext(out var ent, out var animate))
+        {
+            if (Timing.CurTime > animate.EndTime)
+            {
+                _appearanceSystem.SetData(ent, AnimateOnSpawnVisualState.State, false);
+                RemCompDeferred(ent, animate);
+            }
+        }
+    }
+}
