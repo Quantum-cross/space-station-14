afbed77173e85ff443b1313f334607b1ecc10dd0
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-21 22:39:37 -0400

    add animate on spawn component and system

---
+++ Content.Shared/Animations/AnimateOnSpawnComponent.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.Serialization;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared.Animations;
+
+[RegisterComponent]
+[AutoGenerateComponentPause]
+public sealed partial class AnimateOnSpawnComponent : Component
+{
+    [DataField(required: true)]
+    public TimeSpan Delay;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), ViewVariables(VVAccess.ReadWrite)]
+    [AutoPausedField]
+    public TimeSpan EndTime;
+}
+
+[Serializable, NetSerializable]
+public enum AnimateOnSpawnVisualState : byte
+{
+    State,
+}
